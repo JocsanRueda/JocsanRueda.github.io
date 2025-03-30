2c57f06007a93f3f618d70862d661faef306a618
Jocsan Rueda <jocsanelyrueda@gmail.com>
2025-03-29 23:36:46 -0600

    Implement scroll-to-section functionality in MenuBar; navigate to home before scrolling to section

--- src/components/menu-bar.tsx
+++ src/components/menu-bar.tsx
@@ -6,15 +6,19 @@ import {
 } from "@/components/ui/navigation-menu"
 import { Switch } from "@/components/ui/switch"
 import { useTheme } from "./theme-provider"
-import { Link } from "react-scroll"
+import { Link,scroller } from "react-scroll"
 import { data } from "@/data/menu-bar.data"
 import { FaGithub } from "react-icons/fa"
 import { SidebarTrigger } from "./ui/sidebar"
 import { useActiveSection } from "@/context/active-section.context"
 import { cn } from "@/lib/utils"
+import { useLocation, useNavigate } from "react-router-dom"
+import { routeEnum } from "@/shared/route.enum"
 export function MenuBar() {
 
   const { setTheme,theme } = useTheme()
+  const {pathname} = useLocation()
+  const navigate= useNavigate()
 
   const {activeSection, setActiveSection} = useActiveSection()
 
@@ -25,6 +29,22 @@ export function MenuBar() {
       setTheme('dark')
     }
   }
+
+  const handleScrollToSection = (sectionName: string) => {
+  
+    if(pathname!==routeEnum.HOME){
+      navigate(routeEnum.HOME)
+
+      setTimeout(() => {
+        scroller.scrollTo(sectionName, {
+          duration: 500,
+          smooth: true,
+          offset: -41, 
+        });
+        setActiveSection(sectionName); 
+      }, 100);
+    }
+  };
  
   return (
         
@@ -41,7 +61,7 @@ export function MenuBar() {
         {
           data.map((link)=>(
             <NavigationMenuItem key={link.name} className="hidden sm:block">
-              <Link className={cn("font-light data-[active=true]:focus:bg-accent data-[active=true]:hover:bg-accent data-[active=true]:bg-accent/50 data-[active=true]:text-accent-foreground hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground ring-ring/10 dark:ring-ring/20 dark:outline-ring/40 outline-ring/50 [&_svg:not([class*='text-'])]:text-muted-foreground flex flex-col gap-1 rounded-sm p-2 text-sm transition-all focus-visible:ring-4 focus-visible:outline-1 [&_svg:not([class*='size-'])]:size-4 cursor-pointer  ",activeSection===link.url?"font-semibold":"font-light")} to={link.url} smooth={true} duration={500} onSetActive={()=>setActiveSection(link.url)} spy={true} >{link.name}</Link>  </NavigationMenuItem>
+              <Link className={cn("font-light data-[active=true]:focus:bg-accent data-[active=true]:hover:bg-accent data-[active=true]:bg-accent/50 data-[active=true]:text-accent-foreground hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground ring-ring/10 dark:ring-ring/20 dark:outline-ring/40 outline-ring/50 [&_svg:not([class*='text-'])]:text-muted-foreground flex flex-col gap-1 rounded-sm p-2 text-sm transition-all focus-visible:ring-4 focus-visible:outline-1 [&_svg:not([class*='size-'])]:size-4 cursor-pointer  ",activeSection===link.url?"font-semibold":"font-light")} to={link.url} smooth={true} duration={500} onSetActive={()=>setActiveSection(link.url)} spy={true} onClick={()=>handleScrollToSection(link.url)}  >{link.name} </Link>  </NavigationMenuItem>
 
           ))
         }
