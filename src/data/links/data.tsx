c2028c2c773eb8ef6858793362a30f071e595345
Jocsan Rueda <jocsanelyrueda@gmail.com>
2025-04-10 20:50:10 -0600

    Update curriculum link in linksEnum for accuracy

--- src/data/links/data.tsx
+++ src/data/links/data.tsx
@@ -1,6 +1,6 @@
 export enum linksEnum  {
   LINKEDIN="https://www.linkedin.com/in/jocsan-rueda-9b4096218/",
   GITHUB="https://github.com/JocsanRueda",
-  CURIICULUM="https://drive.google.com/file/d/1BakgeZIuuClSYuifRQ9iqmrwM6WWz5Pw/view",
+  CURIICULUM="https://drive.google.com/file/d/1v6kQAzxoIyX-O8w4zwa3seLf1Dh5HWL9/view?usp=sharing",
 
 }
